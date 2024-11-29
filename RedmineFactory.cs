999d918a6bc3c5bff88b446d84408029c9128ab6
John Cone <52921053+JontCont@users.noreply.github.com>
2024-11-29 14:21:57 +0800

    Refactor Redmine integration and enhance .gitignore for IDEs

---
+++ RedmineFactory.cs
@@ -0,0 +1,32 @@
+using System.Collections.Specialized;
+using Redmine.Net.Api;
+using Redmine.Net.Api.Types;
+
+namespace redmineApi;
+
+public class RedmineFactory : RedmineManager
+{
+    static readonly string RedmineApiKey = "KEY";
+    static readonly string RedmineHost   = "HOST";
+    private string? Project { get; set; }
+
+    public RedmineFactory() : base(RedmineHost, RedmineApiKey)
+    {
+    }
+
+    public RedmineFactory(string? project) : base(RedmineHost, RedmineApiKey)
+    {
+        if (string.IsNullOrWhiteSpace(project)) throw new ArgumentNullException(nameof(project));
+        this.Project = project;
+    }
+
+    public IList<Issue> GetIssues(string limit = "10")
+    {
+        return this.GetObjects<Issue>(new NameValueCollection()
+        {
+            { "status_id", "*" },
+            { "project_id", this.Project },
+            { "limit", limit }
+        });
+    }
+}
